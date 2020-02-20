2d195f2eb5cba6ec816613cad841140f7cdc51c1
Vineet-Sharma29 <vineet.s17@iiits.in>
2020-02-20 23:42:36 +0530

    added views.py

--- ipl/views.py
+++ ipl/views.py
@@ -1,7 +1,21 @@
-from django.http import HttpResponse
+from django.http import JsonResponse
 from .models import *
-
+from .util import *
 
 # Create your views here.
 
+
 def index(request):
+    stats = {
+        'runs_list': get_most_runs(),
+        'wickets_list': get_most_wicket(),
+        'fielders_list': get_field_assisters(),
+        'decision': get_toss_descions(),
+        'batting_hand': get_batting_hand(),
+        'bowling_hand': get_bowling_hand(),
+        'scores': get_scores(),
+        'extra_types': get_extra_type(),
+        'dissimal_type': get_dissimal_type(),
+        'total_superovers': get_n_superover()
+    }
+    return JsonResponse(stats)
